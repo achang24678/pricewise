2a44dcc5dc0eeed663d102013bfbc9f6963a501e
Sheng Yu Chang <shengyuchang@Shengs-Air.lan>
2023-10-13 14:01:58 -0700

    first commit

---
+++ components/Searchbar.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (url: string) => {
+    try {
+        const parsedURL = new URL(url);
+        const hostname = parsedURL.hostname;
+
+        // Check if hostname contains amazon.com
+        if (hostname.includes("amazon.com") || hostname.includes("amazon.") || hostname.endsWith("amazon")) {
+            return true;
+        }
+    } catch (error) {
+        return false
+    }
+}
+
+const Searchbar = () => {
+    const [searchPrompt, setSearchPrompt] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+        if (!isValidLink) return alert("Please provide a valid Amazon link")
+
+        try {
+            setIsLoading(true)
+
+            // Scrape the product page
+            const product = await scrapeAndStoreProduct(searchPrompt);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    return (
+        <form
+            className="flex flex-wrap gap-4 mt-12"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                placeholder="Enter product link"
+                className="searchbar-input"
+                onChange={e => setSearchPrompt(e.target.value)}
+                value={searchPrompt}
+            />
+            <button type="submit" className="searchbar-btn" disabled={searchPrompt === ""}>
+                {isLoading ? "Searching..." : "Search"}
+            </button>
+        </form>
+    )
+}
+
+export default Searchbar
